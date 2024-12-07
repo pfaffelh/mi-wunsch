e5d507ddee1fa32c68f067b01fbc05faa0e36ba7
pfaffelh <pfaffelh@gmail.com>
2024-12-07 22:10:18 +0100

    finished furs erste

--- WUNSCH.py
+++ WUNSCH.py
@@ -7,7 +7,7 @@ from itertools import combinations
 from scipy.optimize import linear_sum_assignment
 from config import *
 
-st.set_page_config(page_title="Prüfungseinteilung", layout="wide")
+st.set_page_config(page_title="Prüfungseinteilung", layout="centered")
 
 # Es liegt eine xls-Datei vor mit Feldern
 # Matr, Name, Vorname, Fach, wunsch1, wunsch2, wunsch3
@@ -24,6 +24,8 @@ if "kosten" not in st.session_state:
     st.session_state.kosten = kosten
 if "fixed_seed" not in st.session_state:
     st.session_state.fixed_seed = True
+if "wuensche_xls" not in st.session_state:
+    st.session_state.wuensche_xls = None
 
 def to_excel(df):
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
@@ -60,17 +62,18 @@ with st.expander("Grundeinstellungen"):
     st.session_state.fixed_seed = st.toggle("Es werden immer dieselben Zufallszahlen genommen.", st.session_state.fixed_seed)
 
 with st.expander("Prüfungsslots"):
-    col = st.columns([3,1,1,1])
+    col = st.columns([2,1,1,1])
     col[1].write("#### Analysis")
     col[2].write("#### Lineare Algebra")
     col[3].write("#### Summe")
 
     max_pr = max([p["SlotsAna"]+p["SlotsLA"] for p in st.session_state.pruefer])
-    for i, p in enumerate(st.session_state.pruefer):
+    pr = st.session_state.pruefer
+    for i, p in enumerate(pr):
         col = st.columns([2,1,1,1])
         col[0].write(f"{p['Vorname']} {p['Nachname']}")
-        p['SlotsAna'] = col[1].number_input("Anzahl", min_value = 0, value = p['SlotsAna'], on_change = update_slots, key = f"{p["Kurzname"]}_Ana_size", label_visibility="collapsed")
-        p['SlotsLA'] = col[2].number_input("Anzahl", min_value = 0, value = p['SlotsLA'], args = (i, "Lineare Algebra"), key = f"{p["Kurzname"]}_LA_size", label_visibility="collapsed")
+        p['SlotsAna'] = col[1].number_input("Anzahl", min_value = 0, value = p['SlotsAna'], key = f"{p["Kurzname"]}_Ana_size", label_visibility="collapsed")
+        p['SlotsLA'] = col[2].number_input("Anzahl", min_value = 0, value = p['SlotsLA'], key = f"{p["Kurzname"]}_LA_size", label_visibility="collapsed")
         with col[3]:
             fill_percentage = (p["SlotsAna"] + p["SlotsLA"])/max_pr * 100  # Füllstand in Prozent
             # HTML und CSS für den Balken
@@ -86,15 +89,20 @@ with st.expander("Prüfungsslots"):
     col[2].markdown(sum([ p['SlotsLA'] for p in st.session_state.pruefer]))
     col[3].markdown(sum([ p['SlotsAna'] for p in st.session_state.pruefer]) + sum([ p['SlotsLA'] for p in st.session_state.pruefer]))
 
-with st.expander("Prüferwünsche"):
+    save = st.button("Speichern", type="primary")
+    if save:
+        st.session_state.pruefer = pr
+
+
+with st.expander("Prüferwünsche", expanded = False if st.session_state.wuensche_xls is not None else True):
     st.write("Es wird davon ausgegangen, dass Prüferwünsche in folgendem Format vorliegen:")
     df = pd.read_excel("anmeldungen_bsp.xlsx")
     st.write(df)
-    wuensche_xls = st.file_uploader("Upload Wünsche")
-    wuensche_xls = 'misc/studenten_data.xls'
+    st.session_state.wuensche_xls = st.file_uploader("Upload Wünsche")
+#    wuensche_xls = 'misc/studenten_data.xls'
 
-    if wuensche_xls:
-        df = pd.read_excel(wuensche_xls)
+    if st.session_state.wuensche_xls:
+        df = pd.read_excel(st.session_state.wuensche_xls)
         st.write("Anmeldungen")
         anm = {"Name": "Rohdaten", 
                "Analysis" : df[df["Fach"] == "Analysis"].shape[0], 
@@ -111,11 +119,19 @@ with st.expander("Prüferwünsche"):
 
         anzahl_slots = sum([ p['SlotsAna'] for p in st.session_state.pruefer]) + sum([ p['SlotsLA'] for p in st.session_state.pruefer])
         if df.shape[1] > anzahl_slots:
-            st.write(f"**Einteilung nicht möglich, da es {df.shape[1]} Anmeldungen, aber nur {anzahl_slots} Prüfungsslots gibt.**")
+            st.error(f"**Einteilung nicht möglich, da es {df.shape[1]} Anmeldungen, aber nur {anzahl_slots} Prüfungsslots gibt.**")
         else: 
-            st.write(f"Einteilung möglich, es gibt genug Prüfungsslots.")
+            st.success(f"Einteilung möglich, es gibt genug Prüfungsslots.")
 
-if wuensche_xls:
+        for w1, w2 in combinations(["wunsch1", "wunsch2", "wunsch3"],2):
+            for i in range(df.shape[0]):
+                if df[w1][i] == df[w2][i]:
+                    st.warning(f"{df['Name'][i]}, {df['Vorname'][i]} ({df['Matr'][i]}) hat Prüfer {df[w1][i]} als {w1} und {w2} angegeben. Es wird {w2} ='' gesetzt.")
+                    df[w2][i] = ""
+
+
+
+if st.session_state.wuensche_xls:
     with st.expander("Einteilung"):
         st.write("Nun wird die Einteilung vorgenommen.")
         # Diese Liste von Listen gibt die Wünsche der Studierenden 
@@ -127,15 +143,9 @@ if wuensche_xls:
         if len(fehler):
             st.warning(f"Wünsche {fehler} wurden angegeben, sind aber nicht wählbar!")
 
-        st.write("Doppelte Wünsche werden gelöscht, d.h. wenn wunsch1 == wunsch3, wird wunsch3='' gesetzt.")
-        for wunsch1, wunsch2 in combinations(wuensche,2):
-            for i in range(len(wunsch1)):
-                if wunsch1[i] == wunsch2[i]:
-                    wunsch2[i] = ""
-
-                # spalten sind die workshops der einzelnen Reihen, jede Spalte ist ein Platz im Workshop
         if st.session_state.fixed_seed:
             random.seed(42)
+
         spalten = []
         for p in st.session_state.pruefer:
             spalten.extend([(p["Kurzname"], "Analysis") for i in range(p['SlotsAna'])])
@@ -153,7 +163,7 @@ if wuensche_xls:
 
         row_ind, col_ind = linear_sum_assignment(kostenMatrix, maximize=False)
         df["Prüfer"] = [spalten[j][0] for j in col_ind]
-
+        df = df[["Matr", "Name", "Vorname", "Fach", "Prüfer", "wunsch1", "wunsch2", "wunsch3"]]
 
         st.write("Hier das Ergebnis der Einteilung:")
 
@@ -161,9 +171,8 @@ if wuensche_xls:
             if stu1[1]["Matr"] == stu2[1]["Matr"]:
                 st.write(f"{stu1[1]['Name']}, {stu1[1]['Vorname']} ({stu1[1]['Matr']}) hat sich zu Prüfungen in {stu1[1]['Fach']} und {stu2[1]['Fach']} angemeldet. Prüfer sind {stu1[1]['Prüfer']} und {stu2[1]['Prüfer']}.")    
         
-        st.write(df)
-
         output = BytesIO()
+
         # Streamlit-Button für den Download
         excel_data = to_excel(df)
         st.download_button(
@@ -172,9 +181,41 @@ if wuensche_xls:
             file_name="anmeldungen.xls",
             mime="application/vnd.ms-excel"
         )
+        st.write(df)
 
+        w1 = {"Fach": "Analysis", 
+               "Wunsch 1" : df[(df["Fach"] == "Analysis") & (df["Prüfer"] == df["wunsch1"])].shape[0], 
+               "Wunsch 2" : df[(df["Fach"] == "Analysis") & (df["Prüfer"] == df["wunsch2"])].shape[0], 
+               "Wunsch 3" : df[(df["Fach"] == "Analysis") & (df["Prüfer"] == df["wunsch3"])].shape[0], 
+               "Summe" : df[df["Fach"] == "Analysis"].shape[0]
+               }
+        w2 = {"Fach": "Lineare Algebra", 
+               "Wunsch 1" : df[(df["Fach"] == "Lineare Algebra") & (df["Prüfer"] == df["wunsch1"])].shape[0], 
+               "Wunsch 2" : df[(df["Fach"] == "Lineare Algebra") & (df["Prüfer"] == df["wunsch2"])].shape[0], 
+               "Wunsch 3" : df[(df["Fach"] == "Lineare Algebra") & (df["Prüfer"] == df["wunsch3"])].shape[0], 
+               "Summe" : df[df["Fach"] == "Lineare Algebra"].shape[0]
+               }
+        w3 = {"Fach": "Summe", 
+               "Wunsch 1" : df[df["Prüfer"] == df["wunsch1"]].shape[0], 
+               "Wunsch 2" : df[df["Prüfer"] == df["wunsch2"]].shape[0], 
+               "Wunsch 3" : df[df["Prüfer"] == df["wunsch3"]].shape[0], 
+               "Summe" : df.shape[0]
+               }
+        st.write(pd.DataFrame([w1, w2, w3]))
 
-if wuensche_xls:
+        bel = []
+        for p in st.session_state.pruefer:
+            b = {"Name" : f"{p['Nachname']}, {p['Vorname']}",
+                 "Ana Slots" : p["SlotsAna"],
+                 "Ana Prüfungen" : df[(df["Fach"] == "Analysis") & (df["Prüfer"] == p["Kurzname"])].shape[0],
+                 "LA Slots" : p["SlotsLA"],
+                 "LA Prüfungen" : df[(df["Fach"] == "Lineare Algebra") & (df["Prüfer"] == p["Kurzname"])].shape[0],                 
+            }
+            bel.append(b)
+
+        st.write(pd.DataFrame(bel))
+
+if st.session_state.wuensche_xls:
     with st.expander("Prüferbelastung"):
         anzahl_pruefungen = [sum(df["Prüfer"] == p["Kurzname"]) for p in st.session_state.pruefer]
 #        st.write(anzahl_pruefungen)
