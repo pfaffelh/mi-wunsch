7c2df8928ff846c08a9992b1dd061177fa7e5755
pfaffelh <pfaffelh@gmail.com>
2024-12-18 11:36:30 +0100

    changed f-string12

--- WUNSCH.py
+++ WUNSCH.py
@@ -161,9 +161,9 @@ with st.expander("Upload Daten von Studierenden", expanded = False if st.session
             for item in dict_ilias_la:
                 i = find_item(dict_his, { "MatrikelNr." : item["Matrikelnummer"], "Name" : item["Im Besitz von (Name)"], "Fach" : "Lineare Algebra"})
                 if i >= 0:
-                    dict_his[i]["wunsch1"] = item["Prüfer*in Priorität 1"]
-                    dict_his[i]["wunsch2"] = item["Prüfer*in Priorität 2"]
-                    dict_his[i]["wunsch3"] = item["Prüfer*in Priorität 2"]
+                    dict_his[i]["wunsch1"] = pruefer_kurzname[item["Prüfer*in Priorität 1"]]
+                    dict_his[i]["wunsch2"] = pruefer_kurzname[item["Prüfer*in Priorität 2"]]
+                    dict_his[i]["wunsch3"] = pruefer_kurzname[item["Prüfer*in Priorität 2"]]
                 elif find_item(dict_his, { "MatrikelNr." : item["Matrikelnummer"], "Fach" : "Lineare Algebra"}) != -1:
                     st.warning(f"Matrikelnummer {item['Matrikelnummer']} trägt den falschen Namen.")
                 elif find_item(dict_his, { "Name" : item["Im Besitz von (Name)"], "Fach" : "Lineare Algebra"}) != -1:
